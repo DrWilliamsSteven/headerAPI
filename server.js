930d00ea989635dcb6ab0485781635b14d452851
Steven Williams <dr.williams.steven@gmail.com>
2017-01-09 15:38:55 +0000

    initial commit

--- server.js
+++ server.js
@@ -6,45 +6,17 @@ var path = require("path");
 
 var app = express();
 
-var timeStamp;
 var result;
-
-app.use(express.static(__dirname + '/View'));
-//Store all HTML files in view folder.
-app.use(express.static(__dirname + '/Scripts'));
-//Store all JS and CSS in Scripts folder.
-
-app.get('/',function(request,response){
-  response.sendFile('index.html');
-  //It will find and locate index.html from View or Scripts
-});
 
 app.use(function(request, response, next) {
     
   console.log("In comes a " + request.method + " to " + request.url);
-  var parsedUrl = request.url.slice(1);
-  
-  // if decodeURIComponent(parsedUrl) contains non-numbers, then use as is. -> date.parse
-  // else if only numbers, parseInt * 1000
-    var re = /[^0-9]/g;
-    parsedUrl = re.test(decodeURIComponent(parsedUrl)) ? Date.parse( decodeURIComponent(parsedUrl)) : parseInt(decodeURIComponent(parsedUrl)) * 1000;
- 
-   if (!isNaN( parsedUrl) ){
-        console.log("That's a date!")
-        timeStamp = new Date(parsedUrl);
-        console.log(timeStamp)
-        result = {
-            unix: timeStamp.getTime() / 1000, 
-            natural: timeStamp.toDateString()
+  result = {
+            ipaddress: request.headers['x-forwarded-for'],
+            language: request.headers['accept-language'].split(',')[0], 
+            software: request.headers['user-agent']
             }
             console.log(result)
-    } else {
-        console.log("Sorry! That's not a real date!")
-        result = {
-            unix: null,
-            natural: null
-            }
-    }
   next();
     
 });
@@ -54,7 +26,6 @@ app.use(function(request, response) {
   response.writeHead(200, { "Content-Type": "application/json" });
   response.end(JSON.stringify(result));
 });
-
- http.createServer(app).listen(8080);
+ app.listen(8080);
 
 
